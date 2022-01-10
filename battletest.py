e3361ceea03fdc50d7444d1d72a14fd167421bdf
fireball90 <58610433+fireball90@users.noreply.github.com>
2022-01-10 18:20:30 +0100

    Delete battletest.py

--- battletest.py
+++
@@ -1,9 +0,0 @@
-import battle
-import combat
-import enemy
-import tkinter as tk
-
-player = combat.playerStats(10,100,0)
-enemyName = "redEnemy"
-battlePopUp=battle.battleWindow(player,enemyName)
-battlePopUp.startBattle()
